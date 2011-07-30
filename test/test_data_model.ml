059f888bed432ed4d64910a511a3822862862c0e
Mauricio Fernandez <mfp@acm.org>
2011-07-30 12:05:58 +0200

    Test keyspace management.

--- test/test_data_model.ml
+++ test/test_data_model.ml
@@ -6,6 +6,15 @@ open OUnit
 
 module D = Data_model
 
+let test_keyspace_management db =
+  aeq_list (sprintf "%S") [] (D.list_keyspaces db);
+  let k1 = D.register_keyspace db "foo" in
+  let k2 = D.register_keyspace db "bar" in
+    aeq_list (sprintf "%S") ["bar"; "foo";] (D.list_keyspaces db);
+    aeq_some D.keyspace_name k1 (D.get_keyspace db "foo");
+    aeq_some D.keyspace_name k2 (D.get_keyspace db "bar");
+    return ()
+
 let with_db f () =
   let dir = make_temp_dir () in
   let db = D.open_db dir in
@@ -18,6 +27,7 @@ let test_with_db f = with_db f
 let tests =
   List.map (fun (n, f) -> n >:: test_with_db f)
   [
+    "keyspace management", test_keyspace_management;
   ]
 
 let () =
