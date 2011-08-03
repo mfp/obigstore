f715e4c9b44fca5df9aca330f4d0d27bbed3c741
Mauricio Fernandez <mfp@acm.org>
2011-08-03 16:52:30 +0200

    Benchmark sequential reads.

--- test/benchmark_data_model.ml
+++ test/benchmark_data_model.ml
@@ -49,13 +49,16 @@ let row_data_size_with_colnames (key, cols) =
   row_data_size (key, cols) +
   List.fold_left (fun s c -> s + String.length c.D.Update.name) 0 cols
 
+let pr_separator () =
+  print_endline (String.make 80 '-')
+
 let run_put_colums_bm ~rounds ~iterations ~batch_size ~avg_cols =
   let tmp_dir = Test_00util.make_temp_dir () in
   let db = Data_model.open_db tmp_dir in
   let ks = D.register_keyspace db "ks1" in
     printf "column insertion time (%d row batches, %d columns avg)\n"
       batch_size avg_cols;
-    print_endline (String.make 80 '-');
+    pr_separator ();
     for i = 0 to rounds - 1 do
       let dt =
         Lwt_unix.run (bm_put_columns ~iters:iterations ~batch_size:1000
@@ -85,7 +88,39 @@ let run_put_colums_bm ~rounds ~iterations ~batch_size ~avg_cols =
         (Int64.to_float size_on_disk /. total_data_size');
       tmp_dir
 
+let bm_sequential_read dir =
+  let db = Data_model.open_db dir in
+  let ks = D.register_keyspace db "ks1" in
+  let n_keys = ref 0 in
+  let n_columns = ref 0 in
+  let max_keys = 50 in
+  let rec read_from tx first =
+    lwt (last_key, l) =
+      D.get_slice tx "dummy" ~max_keys ~max_columns:10
+        (D.Data.Key_range { D.Data.first; up_to = None })
+        D.Data.All_columns in
+    let len = List.length l in
+      n_keys := !n_keys + len;
+      n_columns := !n_columns +
+                   List.fold_left (fun s kd -> s + List.length kd.D.Data.columns) 0 l;
+      match last_key with
+        | Some k when len = max_keys -> read_from tx (Some k)
+        | _ -> return () in
+  let dt =
+    Lwt_unix.run
+      (time (fun () -> D.read_committed_transaction ks (fun tx -> read_from tx None)))
+  in
+    printf "Seq read: %9d keys in %8.5fs (%d/s)  \
+            %9d columns in %8.5fs (%d/s)\n%!"
+      !n_keys dt (truncate (float !n_keys /. dt))
+      !n_columns dt (truncate (float !n_columns /. dt))
+
 let () =
   let db_dir =
     run_put_colums_bm ~rounds:10 ~iterations:10000 ~batch_size:1000 ~avg_cols:10
-  in ignore db_dir
+  in
+    Test_00util.keep_tmp := false;
+    print_endline "";
+    for i = 1 to 10 do
+      bm_sequential_read db_dir;
+    done
