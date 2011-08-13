562099670f05be741fad4c83c69b580b24c84dd3
Mauricio Fernandez <mfp@acm.org>
2011-08-13 11:26:33 +0200

    Make sure the responses to sync requests are read in the right order.

--- src/protocol_client.ml
+++ src/protocol_client.ml
@@ -10,13 +10,17 @@ struct
     ich : Lwt_io.input_channel;
     och : Lwt_io.output_channel;
     buf : Bytea.t;
+    mutex : Lwt_mutex.t;
   }
 
   type keyspace = { ks_name : string; ks_id : int; ks_db : db; }
 
   type transaction = keyspace
 
-  let make ich och = { ich; och; buf = Bytea.create 64; closed = false; }
+  let make ich och =
+    { ich; och; buf = Bytea.create 64; closed = false;
+      mutex = Lwt_mutex.create ();
+    }
 
   let close t =
     if not t.closed then
@@ -33,137 +37,143 @@ struct
       Lwt_io.read_into_exactly ich s 0 n >>
       return s
 
-  let get_response f =
-    Lwt_io.atomic
-      (fun ich ->
-         lwt request_id, len, crc = Protocol.read_header ich in
-         let pos = Lwt_io.position ich in
-         (* must read the trailing CRC even if there's an exn in f, lest we
-          * lose synchro *)
-         lwt result =
-           try_lwt
-             lwt x = f ich in
-               return (`OK x)
-           with e -> return (`EXN e) in
-         let pos2 = Lwt_io.position ich in
-         lwt crc2 = read_exactly ich 4 in
-           match result with
-               `OK x ->
-                 let len' = Int64.(to_int (sub pos2 pos)) in
-                   if len' <> len then
-                     raise_lwt (Protocol.Error
-                                  (Protocol.Inconsistent_length (len, len')))
-                   else
-                     (* FIXME: should check CRC2 = CRC(payload) XOR CRC1 *)
-                       return x
-             | `EXN e -> raise_lwt e)
+  let get_response f ich =
+    lwt request_id, len, crc = Protocol.read_header ich in
+    let pos = Lwt_io.position ich in
+    (* must read the trailing CRC even if there's an exn in f, lest we
+     * lose synchro *)
+    lwt result =
+      try_lwt
+        lwt x = f ich in
+          return (`OK x)
+      with e -> return (`EXN e) in
+    let pos2 = Lwt_io.position ich in
+    lwt crc2 = read_exactly ich 4 in
+      match result with
+          `OK x ->
+            let len' = Int64.(to_int (sub pos2 pos)) in
+              if len' <> len then
+                raise_lwt (Protocol.Error
+                             (Protocol.Inconsistent_length (len, len')))
+              else
+                (* FIXME: should check CRC2 = CRC(payload) XOR CRC1 *)
+                  return x
+        | `EXN e -> raise_lwt e
+
+  let request t req f =
+    Lwt_mutex.with_lock t.mutex
+      (fun () -> send_request t req >>
+                 get_response f t.ich)
 
   let list_keyspaces t =
-    send_request t (List_keyspaces { List_keyspaces.prefix = "" }) >>
-    get_response P.read_keyspace_list t.ich
+    request t
+      (List_keyspaces { List_keyspaces.prefix = "" })
+      P.read_keyspace_list
 
   let register_keyspace t name =
-    send_request t (Register_keyspace { Register_keyspace.name; }) >>
-    lwt ks_id = get_response P.read_keyspace t.ich in
-      return { ks_id; ks_name = name; ks_db = t; }
+    lwt ks_id =
+      request t
+        (Register_keyspace { Register_keyspace.name; })
+        P.read_keyspace
+    in return { ks_id; ks_name = name; ks_db = t; }
 
   let get_keyspace t name =
-    send_request t (Get_keyspace { Get_keyspace.name; }) >>
-    match_lwt get_response P.read_keyspace_maybe t.ich with
+    match_lwt
+      request t (Get_keyspace { Get_keyspace.name; }) P.read_keyspace_maybe
+    with
         None -> return None
       | Some ks_id -> return (Some { ks_id; ks_name = name; ks_db = t; })
 
   let keyspace_name ks = ks.ks_name
   let keyspace_id ks = ks.ks_id
 
-  let read_result ks f =
-    get_response f ks.ks_db.ich
+  let request_ks ks req f = request ks.ks_db req f
 
   let list_tables ks =
-    send_request ks.ks_db (List_tables { List_tables.keyspace = ks.ks_id; }) >>
-    read_result ks P.read_table_list
+    request_ks ks (List_tables { List_tables.keyspace = ks.ks_id; }) P.read_table_list
 
   let table_size_on_disk ks table =
-    send_request ks.ks_db
-      (Table_size_on_disk { Table_size_on_disk.keyspace = ks.ks_id; table; }) >>
-    read_result ks P.read_table_size_on_disk
+    request_ks ks
+      (Table_size_on_disk { Table_size_on_disk.keyspace = ks.ks_id; table; })
+      P.read_table_size_on_disk
 
   let key_range_size_on_disk ks ?first ?up_to table =
-    send_request ks.ks_db
+    request_ks ks
       (Key_range_size_on_disk
          { Key_range_size_on_disk.keyspace = ks.ks_id; table;
-           range = { first; up_to; } }; ) >>
-    read_result ks P.read_key_range_size_on_disk
+           range = { first; up_to; } }; )
+      P.read_key_range_size_on_disk
 
   let read_committed_transaction ks f =
+    (* FIXME: should have a recursive mutex and lock it here *)
     send_request ks.ks_db (Begin { Begin.keyspace = ks.ks_id }) >>
-    read_result ks P.read_ok >>
+    get_response P.read_ok ks.ks_db.ich >>
     try_lwt
       lwt y = f ks in
         send_request ks.ks_db (Commit { Commit.keyspace = ks.ks_id }) >>
-        read_result ks P.read_ok >>
+        get_response P.read_ok ks.ks_db.ich >>
         return y
     with e ->
       send_request ks.ks_db (Abort { Abort.keyspace = ks.ks_id }) >>
-      read_result ks P.read_ok >>
+      get_response P.read_ok ks.ks_db.ich >>
       raise_lwt e
 
   let repeatable_read_transaction = read_committed_transaction
 
   let get_keys ks table ?max_keys key_range =
-    send_request ks.ks_db
-      (Get_keys { Get_keys.keyspace = ks.ks_id; table; max_keys; key_range; }) >>
-    read_result ks P.read_keys
+    request_ks ks
+      (Get_keys { Get_keys.keyspace = ks.ks_id; table; max_keys; key_range; })
+      P.read_keys
 
   let count_keys ks table key_range =
-    send_request ks.ks_db
-      (Count_keys { Count_keys.keyspace = ks.ks_id; table; key_range; }) >>
-    read_result ks P.read_key_count
+    request_ks ks
+      (Count_keys { Count_keys.keyspace = ks.ks_id; table; key_range; })
+      P.read_key_count
 
   let get_slice ks table ?max_keys ?max_columns ?(decode_timestamps=false)
     key_range column_range =
-    send_request ks.ks_db
+    request_ks ks
       (Get_slice { Get_slice.keyspace = ks.ks_id; table;
                    max_keys; max_columns; decode_timestamps;
-                   key_range; column_range }) >>
-    read_result ks P.read_slice
+                   key_range; column_range })
+      P.read_slice
 
   let get_slice_values ks table ?max_keys key_range columns =
-    send_request ks.ks_db
+    request_ks ks
       (Get_slice_values { Get_slice_values.keyspace = ks.ks_id; table;
-                          max_keys; key_range; columns; }) >>
-    read_result ks P.read_slice_values
+                          max_keys; key_range; columns; })
+      P.read_slice_values
 
   let get_columns ks table ?max_columns ?(decode_timestamps=false)
         key column_range =
-    send_request ks.ks_db
+    request_ks ks
       (Get_columns { Get_columns.keyspace = ks.ks_id; table;
-                     max_columns; decode_timestamps; key; column_range; }) >>
-    read_result ks P.read_columns
+                     max_columns; decode_timestamps; key; column_range; })
+      P.read_columns
 
   let get_column_values ks table key columns =
-    send_request ks.ks_db
+    request_ks ks
       (Get_column_values { Get_column_values.keyspace = ks.ks_id; table;
-                           key; columns; }) >>
-    read_result ks P.read_column_values
+                           key; columns; })
+      P.read_column_values
 
   let get_column ks table key column =
-    send_request ks.ks_db
-      (Get_column { Get_column.keyspace = ks.ks_id; table; key; column; }) >>
-    read_result ks P.read_column
+    request_ks ks
+      (Get_column { Get_column.keyspace = ks.ks_id; table; key; column; })
+      P.read_column
 
   let put_columns ks table key columns =
-    send_request ks.ks_db
-      (Put_columns { Put_columns.keyspace = ks.ks_id; table; key; columns; }) >>
-    read_result ks P.read_ok
+    request_ks ks
+      (Put_columns { Put_columns.keyspace = ks.ks_id; table; key; columns; })
+      P.read_ok
 
   let delete_columns ks table key columns =
-    send_request ks.ks_db
-      (Delete_columns { Delete_columns.keyspace = ks.ks_id; table; key; columns; }) >>
-    read_result ks P.read_ok
+    request_ks ks
+      (Delete_columns { Delete_columns.keyspace = ks.ks_id; table; key; columns; })
+      P.read_ok
 
   let delete_key ks table key =
-    send_request ks.ks_db
-      (Delete_key { Delete_key.keyspace = ks.ks_id; table; key; }) >>
-    read_result ks P.read_ok
+    request_ks ks
+      (Delete_key { Delete_key.keyspace = ks.ks_id; table; key; })
+      P.read_ok
 end
