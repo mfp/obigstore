6d5672c8f186e0983fdbf0cc52e42fcb6d4c3746
Mauricio Fernandez <mfp@acm.org>
2011-08-14 11:43:31 +0200

    benchmark: run put_columns in parallel to benefit from async reqs.

--- test/benchmark.ml
+++ test/benchmark.ml
@@ -27,7 +27,7 @@ struct
           let rows = List.init batch_size (fun _ -> make_row ()) in
             D.read_committed_transaction ks
               (fun tx ->
-                 Lwt_list.iter_s
+                 Lwt_list.iter_p
                    (fun (key, columns) -> D.put_columns tx table key columns)
                    rows)
         done
