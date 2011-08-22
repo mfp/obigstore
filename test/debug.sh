04d00fec2d0b48c0a84314fcbd9bef2d231198fb
Mauricio Fernandez <mfp@acm.org>
2011-08-22 11:25:55 +0200

    New findlib packages needed in debug.sh.

--- test/debug.sh
+++ test/debug.sh
@@ -2,5 +2,6 @@
 
 CAML_LD_LIBRARY_PATH=../src/libs/ocaml-leveldb/src/:../src \
     rlwrap ocamldebug \
-    `ocamlfind query -recursive -i-format batteries oUnit lwt` \
+    `ocamlfind query -recursive -i-format \
+       batteries oUnit lwt lwt.unix lwt.preemptive threads` \
     -I ../src ./test.run
