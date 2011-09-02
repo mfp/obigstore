9bd3a422ae8aacd31c1aa80f0584a398666411ab
Mauricio Fernandez <mfp@acm.org>
2011-09-02 19:56:21 +0200

    Fix toplevel target and exec script.

--- src/toplevel.sh
+++ src/toplevel.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-CAML_LD_LIBRARY_PATH=libs/ocaml-leveldb/src:. \
+CAML_LD_LIBRARY_PATH=libs/ocaml-leveldb/src:.:core \
   rlwrap ./toplevel \
     -I libs/ocaml-leveldb/src -I libs/extprot/runtime
