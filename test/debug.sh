2afbee6334263861702a1f6e3e977b957908e989
Mauricio Fernandez <mfp@acm.org>
2011-09-05 20:19:38 +0200

    debug.sh: fix include paths.

--- test/debug.sh
+++ test/debug.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-CAML_LD_LIBRARY_PATH=../src/libs/ocaml-leveldb/src/:../src \
+CAML_LD_LIBRARY_PATH=../src/libs/ocaml-leveldb/src/:../src/core \
     rlwrap ocamldebug \
     `ocamlfind query -recursive -i-format \
        batteries oUnit lwt lwt.unix lwt.preemptive threads` \
-    -I ../src ./test.run
+    -I ../src/core \
+    -I ../src/server \
+    -I ../src/client \
+    -I ../src/util \
+    ./test.run
