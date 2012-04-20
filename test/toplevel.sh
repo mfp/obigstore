d7ce77466fdd5e0d853571b845e6a96c10830413
Mauricio Fernandez <mfp@acm.org>
2012-04-20 18:53:35 +0200

    Fix toplevel build and update its include paths.

--- test/toplevel.sh
+++ test/toplevel.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CAML_LD_LIBRARY_PATH=../src/libs/ocaml-leveldb/src/:../src \
+CAML_LD_LIBRARY_PATH=../src/libs/ocaml-leveldb/src/:../src/core \
     rlwrap ./toplevel \
     `ocamlfind query -recursive -i-format batteries oUnit lwt` \
-    -I ../src
+    -I ../src/core -I ../src/client -I ../src/server -I ../src/server/util
