45860913c4cd9ba7fff8817e6860c058c6a8c9fb
Mauricio Fernandez <mfp@acm.org>
2012-05-25 16:48:32 +0200

    bm_read: -latency and -period options.

--- test/bm_read.ml
+++ test/bm_read.ml
@@ -36,6 +36,8 @@ let concurrency = ref 2048
 let multi = ref 1
 let range = ref false
 let time = ref 60.
+let report_latencies = ref false
+let period = ref 5.0
 
 let max_key = ref None
 
@@ -51,6 +53,9 @@ let params = Arg.align
    "-concurrency", Arg.Set_int concurrency,
      "N maximum number of concurrent reads (default: 2048, multi: max 256)";
    "-multi", Arg.Set_int multi, "N Read in N (power of 2) batches (default: 1).";
+   "-latency", Arg.Set report_latencies, " Report latencies.";
+   "-period", Arg.Set_float period,
+     "FLOAT Report stats every FLOAT seconds (default: 5)";
    "-time", Arg.Set_float time, "N Run for N seconds (default: 60).";
   ]
 
@@ -69,6 +74,7 @@ let benchmark_over = ref false
 
 let prev_finished = ref 0
 let t0 = ref 0.
+let exec_start_time = ref 0.
 
 let round_to_pow_of_two n =
   let rec round m =
@@ -77,13 +83,53 @@ let round_to_pow_of_two n =
 
 let report_alarm = ref false
 
+let latencies = ref []
+
+let measure_latency f =
+  if not !report_latencies then f ()
+  else begin
+    let t0 = Unix.gettimeofday () in
+    lwt x = f () in
+    let dt = Unix.gettimeofday () -. t0 in
+      latencies := dt :: !latencies;
+      return x
+  end
+
+let report_header =
+  let n = ref 0 in
+    (fun () ->
+       if !n = 0 then begin
+         printf "#%5s  %6s  " "time" "k rate";
+         if !report_latencies then begin
+           printf "%8s  %8s  %8s  %8s  %8s"
+             "mean" "median" "90th" "95th" "99th"
+         end;
+         printf "\n%!";
+       end;
+       incr n;
+       if !n > 10 then n := 0)
+
 let report () =
   if !report_alarm then begin
+    report_header ();
     let t = Unix.gettimeofday () in
     let dt = t -. !t0 in
-      printf "%d\n%!"
-        (truncate (float (!finished - !prev_finished) /. dt));
+      printf "%-6.2f  %-6d  "
+        (t -. !exec_start_time) (truncate (float (!finished - !prev_finished) /. dt));
       prev_finished := !finished;
+      if !report_latencies && !latencies <> [] then begin
+        let sorted = Array.of_list !latencies in
+        let () = Array.sort compare sorted in
+        let len = Array.length sorted in
+        let mean = Array.fold_left (fun dt s -> s +. dt) 0. sorted /. float len in
+        let median = sorted.(len / 2) in
+        let perc90 = sorted.(90 * len / 100) in
+        let perc95 = sorted.(95 * len / 100) in
+        let perc99 = sorted.(99 * len / 100) in
+          printf "%.6f  %.6f  %.6f  %.6f  %.6f" mean median perc90 perc95 perc99;
+          latencies := [];
+      end;
+      printf "\n%!";
       t0 := t;
       report_alarm := false
   end
@@ -102,7 +148,7 @@ let rec read_values ks =
       incr in_flight;
       begin try_lwt
         let k = mk_key () in
-        lwt _ = C.get_column ks !table k "v" in
+        lwt _ = measure_latency (fun () -> C.get_column ks !table k "v") in
           incr finished;
           report ();
           return ()
@@ -123,7 +169,10 @@ let rec read_values_multi ks =
       incr in_flight;
       begin try_lwt
         let keys = List.init !multi (fun _ -> mk_key ()) in
-        lwt _ = C.get_slice ks !table (`Discrete keys) (`Discrete ["v"]) in
+        lwt _ =
+          measure_latency
+            (fun () -> C.get_slice ks !table (`Discrete keys) (`Discrete ["v"]))
+        in
           finished := !finished + !multi;
           report ();
           return ()
@@ -144,9 +193,10 @@ let rec read_values_seq ?first ks =
       incr in_flight;
       try_lwt
         lwt last_key, l =
-          C.get_slice ks !table ~max_keys:512
-            (`Continuous { DM.first; up_to = None; reverse = false; })
-            `All
+          measure_latency
+            (fun () ->
+               C.get_slice ks !table ~max_keys:512
+                 (`Continuous { DM.first; up_to = None; reverse = false; }) `All)
         in
           (* last one will return fewer than 512, so we "resync" in order for
            * report to work *)
@@ -194,9 +244,10 @@ let () =
         else return ()
     in
       t0 := Unix.gettimeofday ();
+      exec_start_time := !t0;
       ignore
         (let rec report () =
-           Lwt_unix.sleep 5. >> report (report_alarm := true) in report ());
+           Lwt_unix.sleep !period >> report (report_alarm := true) in report ());
       ignore (Lwt_unix.sleep !time >> return (benchmark_over := true));
       if !range then read_values_seq ks
       else if !multi <= 1 then read_values ks
