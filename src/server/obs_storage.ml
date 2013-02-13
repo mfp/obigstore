e663ea54f89a40a7c2422fcd2193b32326ccd2ad
Mauricio Fernandez <mfp@acm.org>
2013-02-13 20:42:08 +0100

    Obs_storage: ensure finalizers are run in main thread.

--- src/server/obs_storage.ml
+++ src/server/obs_storage.ml
@@ -753,7 +753,8 @@ let open_db
           db.slaves;
         lldb
     in
-      Gc.finalise (fun db -> ignore (close_db db)) db;
+      (* must ensure this is run in the main thread *)
+      Obs_finalise.finalise_in_main_thread close_db db;
       reload_keyspaces db lldb;
       db
 
@@ -821,8 +822,11 @@ let clone_keyspace (Proto proto) =
       ks_watches = proto.ks_watches;
       ks_prefix_watches = proto.ks_prefix_watches;
     }
-  in Gc.finalise terminate_keyspace_subs ks;
-     ks
+  in
+    (* must ensure it's run in the main thread *)
+    Obs_finalise.finalise_in_main_thread
+      (fun ks -> terminate_keyspace_subs ks; return ()) ks;
+    ks
 
 let register_keyspace t ks_name =
   try
@@ -1250,7 +1254,8 @@ let remove_watch_if_empty watches table key flag =
   with Not_found -> ()
 
 let setup_flag_key_cleanup watches table key flag =
-  Gc.finalise (remove_watch_if_empty watches table key) flag
+  Obs_finalise.finalise_in_main_thread
+    (fun flag -> remove_watch_if_empty watches table key flag; return ()) flag
 
 let remove_col_watch_if_empty watches table key column flag =
   try
@@ -1266,7 +1271,9 @@ let remove_col_watch_if_empty watches table key column flag =
   with Not_found -> ()
 
 let setup_flag_column_cleanup watches table key column flag =
-  Gc.finalise (remove_col_watch_if_empty watches table key column) flag
+  Obs_finalise.finalise_in_main_thread
+    (fun flag -> remove_col_watch_if_empty watches table key column flag; return ())
+    flag
 
 let get_tainted_flag tx =
   match tx.tainted with
@@ -1308,7 +1315,9 @@ let remove_prefix_watch_if_empty watches table prefix flag =
   with Not_found -> ()
 
 let setup_prefix_flag_cleanup watches table prefix flag =
-  Gc.finalise (remove_prefix_watch_if_empty watches table prefix) flag
+  Obs_finalise.finalise_in_main_thread
+    (fun flag -> remove_prefix_watch_if_empty watches table prefix flag; return ())
+    flag
 
 let watch_prefix tx table prefix =
   match find_maybe tx.ks.ks_tables table with
