04e8fcbe037bca9f182b5c5a3ffa87271f497dfb
Vincent Bernardoff <vb@luminar.eu.org>
2014-07-07 18:55:04 +0200

    Try all address infos returned by getaddrinfo in obigstore
    Previously, when specifying the master's address, only the first one
    was tried before failing, this caused a problem, for example if using
    "localhost", and the master was listening to IPv4 localhost, the
    connection would fail because the client would pick IPv6 localhost by
    default.
    
    This commit also add much refactoring of obigstore.ml, notably
    replacing run_slave by a get_synced_db function, since the code to
    launch the local server is identical, I found it handy to factor it.
    
    Some modernization of Lwt code is done as well (Lwt_unix.run -> Lwt_main.run)
    
    Signed-off-by: Vincent Bernardoff <vb@luminar.eu.org>

--- src/obigstore.ml
+++ src/obigstore.ml
@@ -77,55 +77,49 @@ let open_db dir =
     ~unsafe_mode:!unsafe_mode
     dir
 
-let run_slave ~dir ~address ~data_address ~master_addr ~master_data_addr
+let get_synced_db (ich,och) ~dir ~address ~data_address ~master_data_addr
     auth protos ~role ~password =
   let module C =
     Obs_protocol_client.Make(Obs_protocol_bin.Version_0_0_0) in
   let module DUMP =
     Obs_dump.Make(struct include C include C.Raw_dump end) in
-    Lwt_unix.run begin
-      lwt ich, och = Lwt_io.open_connection master_addr in
-      lwt db = C.make ~data_address:master_data_addr ich och ~role ~password in
-      lwt raw_dump = C.Raw_dump.dump db in
-        DUMP.dump_local ~verbose:true ~destdir:dir raw_dump >>
-        let db = open_db dir in
-          ignore begin try_lwt
-            if !debug then eprintf "Getting replication stream\n%!";
-            lwt stream = C.Replication.get_update_stream raw_dump in
-            if !debug then eprintf "Got replication stream\n%!";
-            let rec get_updates () =
-              match_lwt C.Replication.get_update stream with
-                  None ->
-                    return_unit
-                | Some update ->
-                    lwt s, off, len = C.Replication.get_update_data update in
-                    let () =
-                      if !debug then eprintf "Got update (%d bytes).\n%!" len in
-                    let update' =
-                      Obs_storage.Replication.update_of_string s off len
-                    in
-                      match update' with
-                        None ->
-                          (* FIXME: signal dropped update to master *)
-                          get_updates ()
-                      | Some update' ->
-                          Obs_storage.Replication.apply_update db update' >>
-                          C.Replication.ack_update update >>
-                          get_updates ()
-            in get_updates ()
-          with exn ->
-            (* FIXME: better logging *)
-            let bt = Printexc.get_backtrace () in
-              eprintf "Exception in replication thread: %s\n%s\n%!"
-                (Printexc.to_string exn) bt;
-              return_unit
-          end;
-          S.run_server
-            ~max_async_reqs:!max_concurrency
-            db ~address ~data_address
-            auth protos
-    end
-
+  lwt db = C.make ~data_address:master_data_addr ich och ~role ~password in
+  lwt raw_dump = C.Raw_dump.dump db in
+  DUMP.dump_local ~verbose:true ~destdir:dir raw_dump >>
+  let db = open_db dir in
+  if !debug then eprintf "Getting replication stream\n%!";
+  lwt stream = C.Replication.get_update_stream raw_dump in
+  if !debug then eprintf "Got replication stream\n%!";
+  async (fun () ->
+      try_lwt
+        let rec get_updates () =
+          match_lwt C.Replication.get_update stream with
+          | None ->
+            return_unit
+          | Some update ->
+            lwt s, off, len = C.Replication.get_update_data update in
+            let () =
+              if !debug then eprintf "Got update (%d bytes).\n%!" len in
+            let update' =
+              Obs_storage.Replication.update_of_string s off len
+            in
+            match update' with
+              None ->
+              (* FIXME: signal dropped update to master *)
+              get_updates ()
+            | Some update' ->
+              Obs_storage.Replication.apply_update db update' >>
+              C.Replication.ack_update update >>
+              get_updates ()
+        in get_updates ()
+      with exn ->
+        (* FIXME: better logging *)
+        let bt = Printexc.get_backtrace () in
+        eprintf "Exception in replication thread: %s\n%s\n%!"
+          (Printexc.to_string exn) bt;
+        raise exn
+    );
+  return db
 let bin_protos =
   [
     (0, 0, 0), (module Obs_protocol_bin.Version_0_0_0 : Obs_protocol.SERVER_FUNCTIONALITY);
@@ -150,42 +144,63 @@ let () =
   Lwt_log.default := Lwt_log.channel
                        ~template:"$(date).$(milliseconds) [$(pid)] $(message)"
                        ~close_mode:`Keep ~channel:Lwt_io.stderr ();
-  let get_addresses host port =
-    match Unix.getaddrinfo host port [] with
-    | ai::_ ->
-      (match ai.ai_addr with
-       | Unix.ADDR_INET (h, p) -> Unix.ADDR_INET (h, p), Unix.ADDR_INET (h, p + 1)
-       | _ -> raise (Invalid_argument "Supplied address and/or port cannot be resolved"))
-    | _ -> raise (Invalid_argument "Supplied address and/or port cannot be resolved")
+  let get_address_pairs host port =
+    let open Unix in
+    let ais = getaddrinfo host port [] in
+    try
+      List.map
+        (fun ai -> match ai.ai_addr with
+           | ADDR_INET (h, p) -> ADDR_INET (h, p), ADDR_INET (h, p + 1)
+           | _ -> raise Not_found
+        )
+        ais
+    with Not_found -> []
   in
-  let address, data_address = get_addresses !host !port in
+  let address_pairs = get_address_pairs !host !port in
+  if address_pairs = [] then
+     raise (Invalid_argument
+                     (Printf.sprintf "Impossible to obtain a socket address from %s/%s" !host !port));
+  let address, data_address = List.hd address_pairs in
   let auth = Obs_auth.accept_all in
-    match !db_dir with
-        None -> Arg.usage params usage_message;
+  match !db_dir with
+  | None -> Arg.usage params usage_message;
+    exit 1
+  | Some dir ->
+    begin match !engine with
+        "default" -> ()
+      | "ev" -> Lwt_engine.set (new Lwt_engine.libev)
+      | "select" -> Lwt_engine.set (new Lwt_engine.select)
+      | _ -> Arg.usage params usage_message; exit 1
+    end;
+    Lwt_main.run begin
+      lwt db =
+        match !master with
+        | None ->
+          Lwt.return (open_db dir)
+        | Some master ->
+          let host, port =
+            begin try
+                String.rsplit master ":"
+              with Not_found | Failure _ ->
+                eprintf "-master needs argument of the form HOST:PORT \
+                         (e.g.: 127.0.0.1:15000)\n%!";
                 exit 1
-      | Some dir ->
-          begin match !engine with
-              "default" -> ()
-            | "ev" -> Lwt_engine.set (new Lwt_engine.libev)
-            | "select" -> Lwt_engine.set (new Lwt_engine.select)
-            | _ -> Arg.usage params usage_message; exit 1
-          end;
-          match !master with
-              None ->
-                let db = open_db dir in
-                  Lwt_unix.run (S.run_server db
-                                  ~max_async_reqs:!max_concurrency
-                                  ~replication_wait:!replication_wait
-                                  ~address ~data_address auth protos)
-            | Some master ->
-                let host, port =
-                  begin try
-                      String.rsplit master ":"
-                    with Not_found | Failure _ ->
-                    eprintf "-master needs argument of the form HOST:PORT \
-                             (e.g.: 127.0.0.1:15000)\n%!";
-                    exit 1
-                  end in
-                let master_addr, master_data_addr = get_addresses host port in
-                run_slave ~dir ~address ~data_address ~master_addr ~master_data_addr
-                  auth protos ~role:"guest" ~password:"guest"
+            end in
+          let remote_pairs = get_address_pairs host port in
+          let rec try_connect pairs = match pairs with
+            | [] -> raise_lwt (Failure "No master available")
+            | (a, da)::pairs ->
+              try_lwt Lwt_io.open_connection a >|= fun (ic, oc) -> ic,oc,da with
+              | Unix.Unix_error _ as exn ->
+                if pairs = [] then raise_lwt exn
+                else try_connect pairs
+          in
+          lwt mic, moc, master_data_addr = try_connect remote_pairs in
+          get_synced_db (mic, moc) ~dir ~address ~data_address ~master_data_addr
+            auth protos ~role:"guest" ~password:"guest"
+      in
+      S.run_server db
+        ~max_async_reqs:!max_concurrency
+        ~replication_wait:!replication_wait
+        ~address ~data_address auth protos
+    end
