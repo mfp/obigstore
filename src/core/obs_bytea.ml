56795732b27c0830634f0eeaf67b0f9da10cf339
Mauricio Fernandez <mfp@acm.org>
2015-08-28 22:43:50 +0200

    Obs_bytea: compatibility with extprot 1.2.0.

--- src/core/obs_bytea.ml
+++ src/core/obs_bytea.ml
@@ -17,27 +17,17 @@
 (*                                                                     *)
 (***********************************************************************)
 
-type t = Extprot.Msg_buffer.t =
- {mutable buffer : string;
-  mutable position : int;
-  mutable length : int;
-  initial_buffer : string}
+include Extprot.Msg_buffer
 
-let create n =
- let n = if n < 1 then 1 else n in
- let n = if n > Sys.max_string_length then Sys.max_string_length else n in
- let s = String.create n in
- {buffer = s; position = 0; length = n; initial_buffer = s}
+let create = make
 
 let copy b =
   {
-    initial_buffer = String.create (String.length b.initial_buffer);
     position = b.position;
-    length = b.position;
-    buffer = String.sub b.buffer 0 b.position;
+    length   = b.position;
+    buffer   = String.sub b.buffer 0 b.position;
+    release  = (fun () -> ());
   }
-
-let contents b = String.sub b.buffer 0 b.position
 
 let sub b ofs len =
   if ofs < 0 || len < 0 || ofs > b.position - len
@@ -60,53 +50,8 @@ let nth b ofs =
    invalid_arg "Obs_bytea.nth"
   else String.get b.buffer ofs
 
-let length b = b.position
-
-let clear b = b.position <- 0
-
-let reset b =
-  b.position <- 0; b.buffer <- b.initial_buffer;
-  b.length <- String.length b.buffer
-
-let resize b more =
-  let len = b.length in
-  let new_len = ref len in
-  while b.position + more > !new_len do new_len := 2 * !new_len done;
-  if !new_len > Sys.max_string_length then begin
-    if b.position + more <= Sys.max_string_length
-    then new_len := Sys.max_string_length
-    else failwith "Obs_bytea.add: cannot grow buffer"
-  end;
-  let new_buffer = String.create !new_len in
-  String.blit b.buffer 0 new_buffer 0 b.position;
-  b.buffer <- new_buffer;
-  b.length <- !new_len
-
-let add_char b c =
-  let pos = b.position in
-  if pos >= b.length then resize b 1;
-  b.buffer.[pos] <- c;
-  b.position <- pos + 1
-
-let add_substring b s offset len =
-  if offset < 0 || len < 0 || offset > String.length s - len
-  then invalid_arg "Obs_bytea.add_substring";
-  let new_position = b.position + len in
-  if new_position > b.length then resize b len;
-  String.blit s offset b.buffer b.position len;
-  b.position <- new_position
-
-let add_string b s =
-  let len = String.length s in
-  let new_position = b.position + len in
-  if new_position > b.length then resize b len;
-  String.blit s 0 b.buffer b.position len;
-  b.position <- new_position
-
 let add_bytea b bs =
   add_substring b bs.buffer 0 bs.position
-
-let add_byte b n = add_char b (Char.unsafe_chr n)
 
 let add_vint b n =
   let n = ref n in
